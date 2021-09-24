cc4473a69ee3bb7070570fca77596d3ccbd47b15
Theodorus Clarence <theodorusclarence@gmail.com>
2021-09-24 19:42:43 +0700

    feat: add intro and react

--- theme.config.js
+++ theme.config.js
@@ -1,12 +1,12 @@
 export default {
   github: 'https://github.com/shuding/nextra',
   docsRepositoryBase: 'https://github.com/shuding/nextra/blob/master',
-  titleSuffix: ' – Nextra',
+  titleSuffix: ' – Docs',
   logo: (
     <>
-      <span className="mr-2 font-extrabold hidden md:inline">Nextra</span>
-      <span className="text-gray-600 font-normal hidden md:inline">
-        The Next.js Static Site Generator
+      <span className="hidden mr-2 font-extrabold md:inline">DOCS</span>
+      <span className="hidden font-normal text-gray-600 md:inline">
+        Theodorus Clarence's Documentation
       </span>
     </>
   ),
@@ -16,14 +16,26 @@ export default {
       <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="Nextra: the Next.js site builder" />
-      <meta name="og:description" content="Nextra: the Next.js site builder" />
+      <meta
+        name="description"
+        content="Docs: Theodorus Clarence's Documentation"
+      />
+      <meta
+        name="og:description"
+        content="Docs: Theodorus Clarence's Documentation"
+      />
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:image" content="https://nextra.vercel.app/og.png" />
+      <meta
+        name="twitter:image"
+        content="https://theodorusclarence.com/large-og.jpg"
+      />
       <meta name="twitter:site:domain" content="nextra.vercel.app" />
-      <meta name="twitter:url" content="https://nextra.vercel.app" />
+      <meta name="twitter:url" content="https://docs.theodorusclarence.com/" />
       <meta name="og:title" content="Nextra: Next.js static site generator" />
-      <meta name="og:image" content="https://nextra.vercel.app/og.png" />
+      <meta
+        name="og:image"
+        content="https://theodorusclarence.com/large-og.jpg"
+      />
       <meta name="apple-mobile-web-app-title" content="Nextra" />
       <link
         rel="apple-touch-icon"
@@ -62,6 +74,6 @@ export default {
   nextLinks: true,
   footer: true,
   footerEditLink: 'Edit this page on GitHub',
-  footerText: <>MIT {new Date().getFullYear()} © Nextra.</>,
+  footerText: <>© {new Date().getFullYear()} Theodorus Clarence.</>,
   unstable_faviconGlyph: '👋',
 }
